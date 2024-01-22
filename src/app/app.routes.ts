d698923959c5247194241127d0c507f46501ee69
bokombe <malela.im90@gmail.com>
2024-01-22 16:07:02 +0100

    core terminado sin estilos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { GestionPagesComponent } from './pages/gestion-pages/gestion-pages.component';
+import { HomePagesComponent } from './pages/home-pages/home-pages.component';
+import { ProductsPagesComponent } from './pages/products-pages/products-pages.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePagesComponent },
+  { path: 'gestion', component: GestionPagesComponent },
+  { path: 'productos', component: ProductsPagesComponent },
+];
