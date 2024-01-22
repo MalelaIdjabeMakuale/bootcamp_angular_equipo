9b52ecb4395dbc92f14e5bcc4e736388ab5b059e
Hoja 1 <cordobes_alfonso@hotmail.com>
2024-01-22 16:35:20 +0100

    back-get

--- src/app/pages/products-pages/products-pages.component.ts
+++ src/app/pages/products-pages/products-pages.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Iproducts } from '../../interfaces/iproducts';
+import { ServiceService } from '../../services/service.service';
 
 @Component({
   selector: 'app-products-pages',
@@ -8,5 +10,14 @@ import { Component } from '@angular/core';
   styleUrl: './products-pages.component.css'
 })
 export class ProductsPagesComponent {
+ public productsList:Iproducts [] = []
+
+constructor(private servicio:ServiceService){}
+
+ngOnInit():void{
+this.servicio.getProducts().subscribe((data:any)=>{
+this.productsList = data
+})
+}
 
 }
--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
+  public baseUrl:string="http://localhost:3000/products"
+  public productsUrl: string =`${this.baseUrl}/`
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  public getProducts(){
+    return this.http.get(this.productsUrl)
+  }
 }
