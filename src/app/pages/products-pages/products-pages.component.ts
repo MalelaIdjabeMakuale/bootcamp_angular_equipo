65d803d578fc5134dc8c04740fe3a46890ee9de7
Hoja 1 <cordobes_alfonso@hotmail.com>
2024-01-23 21:29:43 +0100

    fin

--- src/app/pages/products-pages/products-pages.component.ts
+++ src/app/pages/products-pages/products-pages.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, } from '@angular/core';
 import { Iproducts } from '../../interfaces/iproducts';
 import { ServiceService } from '../../services/service.service';
 import { RouterLink } from '@angular/router';
 import { GestionPagesComponent } from '../gestion-pages/gestion-pages.component';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-products-pages',
   standalone: true,
-  imports: [RouterLink,GestionPagesComponent],
+  imports: [RouterLink,GestionPagesComponent,FormsModule,],
   templateUrl: './products-pages.component.html',
   styleUrl: './products-pages.component.css'
 })
 export class ProductsPagesComponent {
  public productsList:Iproducts [] = [];
+ public productsLists:Iproducts [] = [];
 //  public currentPage = 1;
 //  public itemsPerPage = 10;
  public  currentProduc: Iproducts ={
@@ -28,9 +30,39 @@ export class ProductsPagesComponent {
 
 constructor(private servicio:ServiceService){}
 
+
 ngOnInit():void{
 this.servicio.getProducts().subscribe((data:any)=>{
 this.productsList = data
 })
+
+this.servicio.getProducts().subscribe((data:any)=>{
+  this.productsLists = data
+})
+
+
 }
+
+
+  
+
+
+ searchProduct: any =  '';
+ buscados:any[] = [];
+
+ searchProducts(){
+  const lowerCaseTerm=this.searchProduct.toLowerCase();
+
+
+this.productsList = this.productsList.filter( (product: any) => 
+ product.name.toLowerCase().includes(lowerCaseTerm));
+
+
+ }
+
+
+
+
 }
+
+
