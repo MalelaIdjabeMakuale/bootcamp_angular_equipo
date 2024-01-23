6087eeeb6f4562906ab9ae9e72191ae1c6524f9f
bokombe <malela.im90@gmail.com>
2024-01-23 22:12:42 +0100

    fin

--- src/app/pages/products-pages/products-pages.component.ts
+++ src/app/pages/products-pages/products-pages.component.ts
@@ -4,19 +4,21 @@ import { ServiceService } from '../../services/service.service';
 
 import { RouterLink } from '@angular/router';
 import { GestionPagesComponent } from '../gestion-pages/gestion-pages.component';
+import { FormsModule, NgModel } from '@angular/forms';
 
 
 @Component({
   selector: 'app-products-pages',
   standalone: true,
 
-  imports: [RouterLink,GestionPagesComponent],
+  imports: [RouterLink,GestionPagesComponent, FormsModule],
 
   templateUrl: './products-pages.component.html',
   styleUrl: './products-pages.component.css'
 })
 export class ProductsPagesComponent {
  public productsList:Iproducts [] = [];
+ public productsLists:Iproducts [] = [];
 //  public currentPage = 1;
 //  public itemsPerPage = 10;
  public  currentProduc: Iproducts ={
@@ -36,5 +38,27 @@ ngOnInit():void{
 this.servicio.getProducts().subscribe((data:any)=>{
 this.productsList = data
 })
+this.servicio.getProducts().subscribe((data:any)=>{
+  this.productsLists = data
+})
+
+
 }
+
+
+
+
+
+ searchProduct: any =  '';
+ buscados:any[] = [];
+
+ searchProducts(){
+  const lowerCaseTerm=this.searchProduct.toLowerCase();
+
+
+this.productsList = this.productsList.filter( (product: any) => 
+ product.name.toLowerCase().includes(lowerCaseTerm));
+
+
+ }
 }
